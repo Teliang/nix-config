890a1c4f2b645115da14348d73b11e11c0f61464
teliang <telianghung@outlook.com>
2026-04-12 09:39:43 +0000

    core-packages

--- modules/nixos/core-packages.nix
+++ modules/nixos/core-packages.nix
@@ -1,7 +1,76 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-  git
+  noto-fonts
+  noto-fonts-cjk-sans
+  noto-fonts-color-emoji
+  liberation_ttf
+  fira-code
+  fira-code-symbols
+  mplus-outline-fonts.githubRelease
+  dina-font
+  proggyfonts
+
+
+  firefox
+  joplin-desktop
+  vscode
+  google-chrome
+  dbeaver-bin
+  anki-bin
+  android-studio
+  mpv
+  calibre
+  discord
+  gimp
+  obs-studio
+  remmina
+  qbittorrent
+  libreoffice
+  vlc
+  arduino
+  alacritty
+  dconf-editor
+
+
+  jdk
+  nodejs
+  go
+  rustc
+  cargo
+  php
+  python315
+  python314
+  python312
+  python311
+  python310
+  ccls
+  strace
+  gsl
+  boost
+
+  maven
+  gradle
+
+  scrcpy
+  freerdp
   neovim
+  git
+  qemu
+  quickemu
+  wget
+  curl
+
+
+  nload
+  htop
+  btop
+  tree
+  yadm
+  tmux
+  
+  gnomeExtensions.gsconnect
+  gnomeExtensions.appindicator
+  gnomeExtensions.bing-wallpaper
   ];
 }
--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,33 +1,12 @@
-{ ... }:
+{ pkgs,... }:
 {
   
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-
-
-  services.xrdp.enable = true;
-  
-  # Use the GNOME Wayland session
-  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
-  
-  # XRDP needs the GNOME remote desktop backend to function
-  services.gnome.gnome-remote-desktop.enable = true;
-  
-  # Open the default RDP port (3389)
-  services.xrdp.openFirewall = true;
-  
-  # Disable autologin to avoid session conflicts
-  services.displayManager.autoLogin.enable = false;
-  services.getty.autologinUser = null;
-
-  # Disable systemd targets for sleep and hibernation
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
-
+#  services.displayManager.gdm.enable = true;
+#  services.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
 }
