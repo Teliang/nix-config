d6986d104f4c36229a1283c2a76d0dd7044e636d
teliang <telianghung@outlook.com>
2026-04-29 15:25:01 +0900

    rm dirty config

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -6,32 +6,5 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    extraConfig = lib.fileContents ./neovim/init.vim;
-    plugins = with pkgs.vimPlugins; [
-      vim-easy-align
-      # 括号引号补全
-      delimitMate
-      # 注释
-      vim-commentary
-      vim-be-good
-    ];
-
-    coc = {
-      enable = true;
-      package = pkgs.vimPlugins.coc-nvim;
-
-      #    extensions = [
-      #      "coc-json"
-      #      "coc-tsserver"
-      #      "coc-pyright"
-      #      "coc-css"
-      #      "coc-snippets"
-      #      "coc-sh"
-      #      "coc-html"
-      #      "coc-markdownlint"
-      #    ];
-    };
   };
 }
